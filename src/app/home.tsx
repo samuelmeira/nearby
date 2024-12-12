ae6169e7f22a4af9493185fbb39921f8c296260c
Samuel Meira <slmeira.dev@gmail.com>
2024-12-11 21:46:49 -0300

    Last NLW lesson completed

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from "react";
-import { Text, View, Alert } from "react-native";
+import { View, Alert, Text } from "react-native";
 import { api } from "@/services/api";
 import { Categories, CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
+import MapView, { Callout, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { colors, fontFamily } from "@/styles/theme";
+import { router } from "expo-router";
+type MarketsProps = PlaceProps & {
+  latitude: number;
+  longitude: number;
+};
 
-type MarketsProps = PlaceProps & {};
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+};
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
@@ -37,13 +48,26 @@ export default function Home() {
     }
   }
 
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync();
+        console.log(location);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     fetchCategories();
   }, []);
 
   useEffect(() => {
-    fetchMarkets()
-  }, [category])
+    fetchMarkets();
+  }, [category]);
 
   return (
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
@@ -52,7 +76,61 @@ export default function Home() {
         onSelect={setCategory}
         selected={category}
       />
-      <Places data={markets}/>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+
+        {markets.map((item) => (
+          <Marker
+            key={item.id}
+            identifier={item.id}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            image={require("@/assets/pin.png")}
+          >
+            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  {item.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  {item.address}
+                </Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+
+      <Places data={markets} />
     </View>
   );
 }
--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -1,5 +1,6 @@
 import { Text, useWindowDimensions } from "react-native";
 import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { router } from "expo-router";
 
 import { Place, PlaceProps } from "../place";
 import { useRef } from "react";
@@ -28,7 +29,7 @@ export function Places({ data }: Props) {
       <BottomSheetFlatList
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Place data={item} />}
+        renderItem={({ item }) => <Place data={item} onPress={() => router.navigate(`/market/${item.id}`)}/>}
         contentContainerStyle={s.content}
         ListHeaderComponent={() => (
           <Text style={s.title}>Explore locais perto de você</Text>
